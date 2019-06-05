6e49799948a741b1cba0f47207cf3c80255f09f4
polynova <jocelyncasanova1@gmail.com>
2019-06-05 19:03:34 -0400

    currently at step spliting words and word count

--- word_frequency.py
+++ word_frequency.py
@@ -3,11 +3,39 @@ STOP_WORDS = [
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+def clean_text(text):
+    """ Removes punctuation from file """
+    text = text.replace(",","").replace(".", "").replace("!", "")
+    text = text.lower()
+    for word in text:
+        if word in STOP_WORDS:
+            text = text.replace(word, "")
+    return text 
+
+    
+    
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    # pass
+    #open read file 
+    #remove punctuation 
+    #lowercase
+    #remove stop words that are in this file above
+    #keep count of how often words are used
+    #use dictionary to print out frequency
+    #calculate frequency 
+
+
+    with open (file) as my_file:
+        my_file = my_file.read()
+    my_file = clean_text(my_file) 
+    print(my_file)
+    
+
+
+    
 
 
 if __name__ == "__main__":
